a4745b7f6c43eb0e2d891823aa8262d55f0ffa51
drmadwill <nofelsalahov9@gmail.com>
2024-03-11 11:34:38 +0400

    add CrosExtension

---
+++ DrMadWill.Extensions.Cros/DrMadWill.Extensions.Cros/CrosExtension.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DrMadWill.Extensions.Cros;
+
+public static class CrosExtension
+{
+    private static  string _crosName;
+
+    public static IServiceCollection AddCrosServices(this IServiceCollection services,string[] serviceUrls,string crosName =  "AllowSpecificOrigin")
+    {
+        _crosName = crosName;
+        services.AddCors(options =>
+        {
+            options.AddPolicy(_crosName,
+                builder => builder.WithOrigins( (serviceUrls))
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+        });
+
+        return services;
+    }
+    
+    public static IServiceCollection AddCrosServices(this IServiceCollection services,IConfiguration configuration,string crosName =  "AllowSpecificOrigin",string configurationName = "ServiceUrls")
+    {
+        _crosName = crosName;
+        var serviceUrls =  configuration.GetSection(configurationName).Get<string[]>();
+        
+        services.AddCors(options =>
+        {
+            options.AddPolicy(_crosName,
+                builder => builder.WithOrigins( (serviceUrls))
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+        });
+
+        return services;
+    } 
+
+    public static IApplicationBuilder UsingCrosServices(IApplicationBuilder app)
+    {
+        app.UseCors(_crosName);
+        return app;
+    }
+
+
+
+}
--- DrMadWill.Extensions.Response/DrMadWill.Extensions.Response/ServiceResult.cs
+++ DrMadWill.Extensions.Response/DrMadWill.Extensions.Response/ServiceResult.cs
@@ -23,10 +23,43 @@ public class ServiceResult
         Errors = errors;
     }
 
-    public static ServiceResult Failed(string? message = null, string[]? errors = null) =>
-        new ServiceResult(false, message, errors);
+    public static ServiceResult Failed(string? message = null, string[]? errors = null) => new(false, message, errors);
     
-    public static ServiceResult Succeed( string? message = null)
-         => new ServiceResult(true, message);
+    public static ServiceResult Succeed(string? message = null) => new(true, message);
+    
+}
+
+public class GenericServiceResult<T>
+{
+    public T Result { get; set; }
+    public bool Success { get; }
+    public bool Fail => !Success;
+    public bool ResultNotNull => Result != null;
+    public string? Message { get; set; }
+    public string[]? Errors { get; set; }
+    
+    public GenericServiceResult(T result,bool status, string? message)
+    {
+        Result = result;
+        Success = status;
+        Message = message;
+    }
+    
+    public GenericServiceResult()
+    {
+    }
+    
+    public GenericServiceResult(T result,bool status, string? message, string[]? errors)
+    {
+        Result = result;
+        Success = status;
+        Message = message;
+        Errors = errors;
+    }
+
+    public static GenericServiceResult<T> Failed(T result, string? message = null, string[]? errors = null) =>
+        new(result, false, message, errors);
+    
+    public static GenericServiceResult<T> Succeed(T result, string? message = null) => new(result, true, message);
     
 }
